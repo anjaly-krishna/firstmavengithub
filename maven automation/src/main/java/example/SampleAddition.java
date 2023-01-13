7b8dee3f703e2bc8282f90975e8b813c0cab80d9
anjaly-krishna <122580410+anjaly-krishna@users.noreply.github.com>
2023-01-13 07:37:44 +0530

    upload my pjt

---
+++ maven automation/src/main/java/example/SampleAddition.java
@@ -0,0 +1,15 @@
+package example;
+
+public class SampleAddition {
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=5;
+		int c=a+b;
+		System.out.println("sum="+c);
+		
+
+	}
+
+}
